48c167e2ddd87670d929c1612f0567885a09a2a8
Achmad Ali Akbar <114000864+barcodew@users.noreply.github.com>
2022-12-10 22:36:49 +0800

    Create tebak_ganjil_genap.py

---
+++ tebak_ganjil_genap.py
@@ -0,0 +1,18 @@
+import os
+
+def fungsi(bil):
+    if bil %2 == 0:
+        print("Bilangan Yang Anda Masukkan Adalah Genap!")
+    else:
+        print("Bilangan Yang Anda Masukkan Adalah Ganjil!")
+
+
+while True:
+
+    bilangan = int(input("Masukkan Bilangan :"))
+    fungsi(bilangan)
+    njut = input("Ingin Melanjutkan Program? (y/n) :")
+    os.system("cls")
+    if njut == "n" or njut == "N":
+        break
+ print("Program Selesai!, Semoga Harimu Suram.")
