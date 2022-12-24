172d365f81aec611912e8f44ceb19dc2effeb8b5
Achmad Ali Akbar <114000864+barcodew@users.noreply.github.com>
2022-12-24 12:38:01 +0800

    Create pyramid_angka.py

---
+++ pyramid_angka.py
@@ -0,0 +1,11 @@
+def print_pyramid(n):
+  for i in range(1, n+1):
+    for j in range(n-i):
+      print(" ", end="")
+    for j in range(i, 2*i):
+      print(j, end="")
+    for j in range(2*i-2, i-1, -1):
+      print(j, end="")
+    print()
+
+print_pyramid(6)
